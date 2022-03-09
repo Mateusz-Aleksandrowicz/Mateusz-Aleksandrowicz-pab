146f8df22948402f2990831fefdf8289ea1fde90
mateusz.aleksandrowi <mateusz.aleksandrowi@PK6-20>
2022-03-09 19:49:37 +0100

    first step to finish

--- projekt1/index.js
+++ projekt1/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
-app.get('/', function (req, res) {
-    res.send('Hello World');
+app.get('/api/:operation/:num1/:num2', function (req, res) {
+    var operation = req.params.operation;
+    var num1 = parseInt(req.params.num1);
+    var num2 = parseInt(req.params.num2);
+    var resutl = num1 + num2;
+    res.send(resutl.toString());
 });
 app.listen(3000);
--- projekt1/index.ts
+++ projekt1/index.ts
@@ -1,6 +1,11 @@
 const express = require('express')  
 const app = express()  
-app.get('/', function (req, res) {  
-  res.send('Hello World')  
+app.get('/api/:operation/:num1/:num2', function (req, res) {  
+  let operation = req.params.operation;
+  let num1 = parseInt(req.params.num1);
+  let num2 = parseInt(req.params.num2);
+
+  let result = num1 + num2;
+  res.send(result.toString());  
 })  
 app.listen(3000) 
