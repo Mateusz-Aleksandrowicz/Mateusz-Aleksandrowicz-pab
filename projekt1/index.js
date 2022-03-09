03c760680e3716d8ca31c93626cbeaa93464e75c
mateusz.aleksandrowi <mateusz.aleksandrowi@PK6-20>
2022-03-09 19:56:43 +0100

    finish

--- projekt1/index.js
+++ projekt1/index.js
@@ -4,7 +4,21 @@ app.get('/api/:operation/:num1/:num2', function (req, res) {
     var operation = req.params.operation;
     var num1 = parseInt(req.params.num1);
     var num2 = parseInt(req.params.num2);
-    var resutl = num1 + num2;
-    res.send(resutl.toString());
+    if (operation == "dodaj") {
+        var result = num1 + num2;
+        res.send("".concat(num1, " + ").concat(num2, " = ").concat(result.toString()));
+    }
+    if (operation == "usun") {
+        var result = num1 - num2;
+        res.send("".concat(num1, " - ").concat(num2, " = ").concat(result.toString()));
+    }
+    if (operation == "podziel") {
+        var result = num1 / num2;
+        res.send("".concat(num1, " / ").concat(num2, " = ").concat(result.toString()));
+    }
+    if (operation == "pomnoz") {
+        var result = num1 * num2;
+        res.send("".concat(num1, " * ").concat(num2, " = ").concat(result.toString()));
+    }
 });
 app.listen(3000);
