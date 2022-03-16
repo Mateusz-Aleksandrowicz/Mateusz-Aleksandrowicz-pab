d57a960e47854db9ebdb1338504412a9ca820508
matek1.aleksandrowicz@gmail.com <matek1.aleksandrowicz@gmail.com>
2022-03-17 00:08:14 +0100

    adding simple validation

--- projekt2/index.ts
+++ projekt2/index.ts
@@ -17,14 +17,27 @@ app.get('/', function (req: Request, res: Response) {
 
 app.post('/note', function (req: Request, res: Response) {
 
+    const title = req.body.title
+    const content = req.body.content
     const note = req.body;
-    const noteId = uuidv4(); // random id
-    const noteWithId = { ...note, id: noteId }; // dodanie do obiektu "note" nowego param "id"
 
-    notes.push(noteWithId);
+    if (title === undefined) {
+        res.status(404).send('Podaj poprawny tytuł!')
+        console.log("Podaj poprawny tytuł!")
+    }
+    else if (content === undefined) {
+        res.status(404).send('Podaj poprawna tresc!')
+        console.log("Podaj poprawna tresc!")
+    }
+    else {
+        const noteId = uuidv4(); // random id
+        const noteWithId = { ...note, id: noteId }; // dodanie do obiektu "note" nowego param "id"
 
-    console.log(req.body) // e.x. req.body.title 
-    res.status(200).send('POST Hello World')
+        notes.push(noteWithId);
+
+        console.log(req.body) // e.x. req.body.title 
+        res.status(200).send('POST Hello World')
+    }
 })
 
 app.listen(3000)
