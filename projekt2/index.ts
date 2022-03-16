dbc98f24f597c951974ac60644fe2be48ad2cd97
matek1.aleksandrowicz@gmail.com <matek1.aleksandrowicz@gmail.com>
2022-03-16 23:43:17 +0100

    post, get funcionality with random id

--- projekt2/index.ts
+++ projekt2/index.ts
@@ -1,14 +1,28 @@
 import express from 'express'
 import { Request, Response } from 'express'
+import { Note } from '/GithubRepositories/Mateusz-Aleksandrowicz-pab/projekt2/note'
+import { v4 as uuidv4 } from 'uuid'
 
 const app = express()
 
 app.use(express.json())
 
+const notes: Note[] = []
+
 app.get('/', function (req: Request, res: Response) {
-    res.send('GET Hello World')
+
+    res.send(notes)
+    console.log(notes)
 })
-app.post('/', function (req: Request, res: Response) {
+
+app.post('/note', function (req: Request, res: Response) {
+
+    const note = req.body;
+    const noteId = uuidv4(); // random id
+    const noteWithId = { ...note, id: noteId }; // dodanie do obiektu "note" nowego param "id"
+
+    notes.push(noteWithId);
+
     console.log(req.body) // e.x. req.body.title 
     res.status(200).send('POST Hello World')
 })
