88bde3f09ec721ae6f257935857ea0960da8ae44
matek1.aleksandrowicz@gmail.com <matek1.aleksandrowicz@gmail.com>
2022-03-19 13:47:47 +0100

    adding put, finish api

--- projekt2/index.ts
+++ projekt2/index.ts
@@ -12,25 +12,25 @@ app.use(express.json())
 
 const notes: Note[] = []
 
-app.get('/get/note', function (req: Request, res: Response) { // getting all notes
+app.get('/note', function (req: Request, res: Response) { // getting all notes
     res.status(200).send(notes)
 })
 
-app.get('/get/note/:id', function (req: Request, res: Response) { // getting single note by id
+app.get('/note/:id', function (req: Request, res: Response) { // getting single note by id
     const id = req.params.id
     const result = notes.find(el => el.id === id)
 
-    if(result){
+    if (result) {
         res.status(200).send(result)
         console.log(result)
     }
-    else{
+    else {
         res.status(404).send("Notatka nie istnieje")
     }
-    
+
 })
 
-app.post('/post/note', function (req: Request, res: Response) {
+app.post('/note', function (req: Request, res: Response) {
 
     const id = req.body.id
     const title = req.body.title
@@ -39,7 +39,7 @@ app.post('/post/note', function (req: Request, res: Response) {
     const date_ob = new Date().toISOString();
     const note = req.body;
 
-    if(createDate === undefined){
+    if (createDate === undefined) {
         createDate = date_ob
     }
 
@@ -53,27 +53,56 @@ app.post('/post/note', function (req: Request, res: Response) {
         console.log("Podaj poprawna tresc!")
     }
 
-        const noteId = uuidv4() // random id
-        const noteFinish = { ...note, id: noteId, createDate: createDate} // dodanie do obiektu "note" nowego param "id", "date"
-        notes.push(noteFinish)
+    const noteId = uuidv4() // random id
+    const noteFinish = { ...note, id: noteId, createDate: createDate } // dodanie do obiektu "note" nowego param "id", "date"
+    notes.push(noteFinish)
 
-        console.log(noteFinish) 
-        res.status(201).send(noteFinish)
-        console.log(notes)
+    console.log(noteFinish)
+    res.status(201).send(noteFinish)
+    console.log(notes)
 })
 
-app.delete('/delete/note/:id', function (req: Request, res: Response) {
+app.delete('/note/:id', function (req: Request, res: Response) {
 
     const id = req.params.id
     const result = notes.find(el => el.id === id) // znalezienie note z danym id
     const index = notes.indexOf(result!, 0);
 
     if (index > -1) {
-    notes.splice(index, 1);
-}
-    res.send(notes)
+        notes.splice(index, 1);
+    }
+    res.status(204).send(notes)
     console.log(notes)
-    
+
+})
+
+app.put('/note/:id', function (req: Request, res: Response) {
+    const id = req.params.id
+    const changeNote = req.body
+    const title = req.body.title
+    const content = req.body.content
+
+    const result = notes.find(el => el.id === id)
+    const index = notes.indexOf(result!, 0);
+
+    if (title === undefined) {
+        res.status(404).send('Podaj poprawny tytuł!')
+        console.log("Podaj poprawny tytuł!")
+    }
+
+    if (content === undefined) {
+        res.status(404).send('Podaj poprawna tresc!')
+        console.log("Podaj poprawna tresc!")
+    }
+
+    if (index > -1) {
+        notes[index] = changeNote
+        res.status(204).send(notes)
+    }
+    else {
+        res.status(404).send("Notatka nie istnieje")
+    }
+    console.log(notes)
 })
 
 app.listen(3000)
